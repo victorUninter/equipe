d11c16f178963e8d9bcf55c674fb20da7ad01412
victorUninter <153622519+victorUninter@users.noreply.github.com>
2024-02-12 17:41:31 -0300

    Update gestaoEquipe.py

--- gestaoEquipe.py
+++ gestaoEquipe.py
@@ -120,20 +120,18 @@ def run():
         # atualizar = st.button('ATUALIZAR',type="primary")
         # Verifica se o botão de atualização foi clicado
         if st.button("ATUALIZAR", key="atualizar_button", type="primary"):
-            with st.form(key='confirm_form'):
-                matricula = st.text_input("Digite sua matrícula:")
-                confirmar = st.form_submit_button("Confirmar")
+            matricula = st.text_input("Digite sua matrícula:")
+            confirmar = st.button("Confirmar")
 
-                if confirmar:
-                    matribase = len(baseCompleta[baseCompleta['MATRICULA'].str.contains(str(matricula))])
+            if confirmar:
+                matribase = len(baseCompleta[baseCompleta['MATRICULA'].str.contains(str(matricula))])
 
-                    if matribase >= 1:
-                        # Atualiza a variável global baseCompleta
-                        baseCompleta = atualizaBanco(edited_df, baseCompleta)
-                        st.success('Atualizado com sucesso!', icon="✅")
-                    else:
-                        st.warning("Matrícula inválida ou processo cancelado.")
-
+                if matribase >= 1:
+                    # Atualiza a variável global baseCompleta
+                    baseCompleta = atualizaBanco(edited_df, baseCompleta)
+                    st.success('Atualizado com sucesso!', icon="✅")
+                else:
+                    st.warning("Matrícula inválida ou processo cancelado.")
     # if atualizar:
     #     atualizaBanco(edited_df,baseCompleta)
     #     st.success('Atualizado com sucesso!', icon="✅")
